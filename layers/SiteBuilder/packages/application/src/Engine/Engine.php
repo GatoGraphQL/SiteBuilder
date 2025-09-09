f44dc258dacf4f0b9b2b8c3abfd8bdff685e022d
leoloso <leo@getpop.org>
2025-09-09 16:34:39 +0800

    moved RemoveEntryComponentFromOutputEngineTrait

--- layers/SiteBuilder/packages/application/src/Engine/Engine.php
+++ layers/SiteBuilder/packages/application/src/Engine/Engine.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PoP\Application\Engine;
 
-use PoPAPI\API\Engine\RemoveEntryComponentFromOutputEngineTrait;
 use PoP\ConfigurationComponentModel\Engine\Engine as UpstreamEngine;
 
 class Engine extends UpstreamEngine
